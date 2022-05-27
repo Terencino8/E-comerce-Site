28df7ef439a236ce1fb6b47cd6ee9fa3f481aafc
Terence <terencino8@gmail.com>
2022-05-27 16:20:35 +0100

    i added the product details page, the cart page, the account page is still under construction will be done soon

--- js/ecommerce.js
+++ js/ecommerce.js
@@ -12,3 +12,26 @@ function menutoggle(){
         MenuItems.style.maxHeight = "0px";
     } 
 }
+
+
+
+// js for product gallery
+
+var productImg =document.getElementById("productImg");
+var SmallImg = document.getElementsByClassName("small-img");
+
+SmallImg[0].onclick = function(){
+    productImg.src =  SmallImg[0].src;
+}
+
+SmallImg[1].onclick = function(){
+    productImg.src =  SmallImg[1].src;
+}
+
+SmallImg[2].onclick = function(){
+    productImg.src =  SmallImg[2].src;
+}
+
+SmallImg[3].onclick = function(){
+    productImg.src =  SmallImg[3].src;
+}
